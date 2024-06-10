b3b5d42ae19640970f8232f2d6263be92e75f504
Maiker Gonzales <maiker.gonzales@vallegrande.edu.pe>
2024-06-10 13:17:01 -0500

    dockerizing services and updating config-data

---
+++ product-service/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO productos (productId, name, description, price, createdAt, updatedAt)
+VALUES
+    (1, 'Producto 1', 'Descripción del producto 1', 10.50, '2022-06-01 08:00:00', '2022-06-01 08:00:00'),
+    (2, 'Producto 2', 'Descripción del producto 2', 15.75, '2022-06-02 09:30:00', '2022-06-02 09:30:00'),
+    (3, 'Producto 3', 'Descripción del producto 3', 20.00, '2022-06-03 10:45:00', '2022-06-03 10:45:00'),
+    (4, 'Producto 4', 'Descripción del producto 4', 25.25, '2022-06-04 11:15:00', '2022-06-04 11:15:00'),
+    (5, 'Producto 5', 'Descripción del producto 5', 30.50, '2022-06-05 12:00:00', '2022-06-05 12:00:00');
